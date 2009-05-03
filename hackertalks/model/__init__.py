2f4f444d421573aa1a6a7a67dabbe171b5650868
George Shammas <georgyo@gmail.com>
2009-05-02 20:16:47 -0400

    New Models\!

--- hackertalks/model/__init__.py
+++ hackertalks/model/__init__.py
@@ -2,6 +2,7 @@
 import sqlalchemy as sa
 from sqlalchemy import orm
 from sqlalchemy.ext.declarative import declarative_base
+from datetime import datetime
 
 from hackertalks.model import meta
 
@@ -21,57 +22,47 @@ def init_model(engine):
 
 
 talks_tags_table = sa.Table('talks_tags', meta.metadata,
-    sa.Column('talk_id', sa.types.Integer, sa.ForeignKey('talks.id'), primary_key=True),
-    sa.Column('tag_id', sa.types.Integer, sa.ForeignKey('tags.id'), primary_key=True),
+    sa.Column('talk_id', sa.types.Integer(), sa.ForeignKey('talks.id'), primary_key=True),
+    sa.Column('tag_id', sa.types.Integer(), sa.ForeignKey('tags.id'), primary_key=True),
     )
 
 talks_speakers_table = sa.Table('talks_speakers', meta.metadata,
-    sa.Column('talk_id', sa.types.Integer, sa.ForeignKey('talks.id'), primary_key=True),
-    sa.Column('speaker_id', sa.types.Integer, sa.ForeignKey('speakers.id'), primary_key=True),
+    sa.Column('talk_id', sa.types.Integer(), sa.ForeignKey('talks.id'), primary_key=True),
+    sa.Column('speaker_id', sa.types.Integer(), sa.ForeignKey('speakers.id'), primary_key=True),
     )
 
-class Talk(Base):
-    __tablename__ = 'talks'
+class TalkType(Base):
+    __tablename__ = 'talk_types'
 
-    id = sa.Column(sa.types.Integer, primary_key=True)
-    title = sa.Column(sa.types.String)
-    description = sa.Column(sa.types.String)
-    language_code = sa.Column(sa.types.String)
-    video_length = sa.Column(sa.types.String)
-    video_embedcode = sa.Column(sa.types.String)
+    id = sa.Column(sa.types.Integer(), primary_key=True)
+    type = sa.Column(sa.types.Unicode())
 
-    tags =  orm.relation('Tag', secondary=talks_tags_table, backref='talks')
-    speakers = orm.relation('Speaker', secondary=talks_speakers_table, backref='posts')
-
-    def __init__(self, title, description, language_code, video_length, video_embedcode):
-        self.title = title
-        self.description = description
-        self.language_code = language_code
-        self.video_length = video_length
-        self.video_embedcode = video_embedcode
+    def __init__(self, type):
+        self.type = type
 
     def __repr__(self):
-        return "<Talk('%s', '%s', '%s', '%s', '%s')>" % (self.title, self.description, self.language_code, self.video_length, self.video_embedcode)
+        return "<TalkType('%s')>" % (self.type)
+
 
 class Tag(Base):
     __tablename__ = 'tags'
 
-    id = sa.Column(sa.types.Integer, primary_key=True)
-    tag = sa.Column(sa.types.String)
+    id = sa.Column(sa.types.Integer(), primary_key=True)
+    tag = sa.Column(sa.types.Unicode())
 
     def __init__(self, tag):
         self.tag = tag
 
     def __repr__(self):
         return "<Tag('%s')>" % (self.tag)
-        
+
 class Speaker(Base):
     __tablename__ = 'speakers'
 
-    id = sa.Column(sa.types.Integer, primary_key=True)
-    first_name = sa.Column(sa.types.String)
-    last_name = sa.Column(sa.types.String)
-    nickname = sa.Column(sa.types.String)
+    id = sa.Column(sa.types.Integer(), primary_key=True)
+    first_name = sa.Column(sa.types.Unicode())
+    last_name = sa.Column(sa.types.Unicode())
+    nickname = sa.Column(sa.types.Unicode())
 
     def __init__(self, first_name, last_name, nickname):
         self.first_name = first_name
@@ -84,8 +75,8 @@ class Speaker(Base):
 class Language(Base):
     __tablename__ = 'languages'
 
-    code = sa.Column(sa.types.String, primary_key=True)
-    name = sa.Column(sa.types.String)
+    code = sa.Column(sa.types.Unicode(), primary_key=True)
+    name = sa.Column(sa.types.Unicode())
 
     def __init__(self, code, name):
         self.code = code
@@ -94,4 +85,29 @@ class Language(Base):
     def __repr__(self):
         return "<Lanuage('%s', '%s')>" % (self.code, self.name)
 
+class Talk(Base):
+    __tablename__ = 'talks'
+
+    id = sa.Column(sa.types.Integer(), primary_key=True)
+    title = sa.Column(sa.types.Unicode())
+    description = sa.Column(sa.types.UnicodeText())
+    video_length = sa.Column(sa.types.Interval())
+    video_embedcode = sa.Column(sa.types.Unicode())
+    # Many To One Columns
+    language_code = sa.Column(sa.types.Unicode(), sa.ForeignKey('languages.code'))
+    type_id = sa.Column(sa.types.Integer(), sa.ForeignKey('talk_types.id'))
+    #Many to One Relationships
+    type = orm.relation(TalkType, backref=orm.backref('talk_types', order_by=id))
+    language = orm.relation(Language, backref=orm.backref('languages', order_by=id))
+    #Many to Many  Relationships
+    tags =  orm.relation('Tag', secondary=talks_tags_table, backref='talks')
+    speakers = orm.relation('Speaker', secondary=talks_speakers_table, backref='talks')
+
+    def __init__(self, title, description, language_code, length):
+        self.title = title
+        self.description = description
+        self.length = length
+
+    def __repr__(self):
+        return "<Talk('%s', '%s', '%s', '%s', '%s')>" % (self.title, self.description, self.length, self.type, self.language)
 
