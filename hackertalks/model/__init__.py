57ba7fa27a840d8b6c09e8a549882fba41aa40a8
George Shammas <georgyo@gmail.com>
2009-08-29 20:59:39 -0400

    Added the license table, and made some minor changes types

--- hackertalks/model/__init__.py
+++ hackertalks/model/__init__.py
@@ -23,8 +23,8 @@ class Speaker(Base):
     __tablename__ = 'speakers'
 
     id = sa.Column(sa.types.Integer, primary_key=True)
-    name = sa.Column(sa.types.Unicode)
-    job_title = sa.Column(sa.types.Unicode)
+    name = sa.Column(sa.types.Unicode())
+    job_title = sa.Column(sa.types.Unicode())
 
     def __init__(self, name, job_title):
         self.name = name
@@ -32,24 +32,52 @@ class Speaker(Base):
 
     def __repr__(self):
         return "<Speaker(id='%d', name='%s')>" % (self.id, self.name)
+    
+class License(Base):
+    __tablename__ = 'licenses'
+    
+    id = sa.Column(sa.types.Integer, primary_key=True)
+    name = sa.Column(sa.types.Unicode())
+    shortname = sa.Column(sa.types.Unicode())
+    abbreviation = sa.Column(sa.types.Unicode())
+    link = sa.Column(sa.types.Unicode())
+    thumbnail = sa.Column(sa.types.Unicode())
+    shareable = sa.Column(sa.types.Boolean())
+    description = sa.Column(sa.types.UnicodeText())
+    
+    def __init__(self, name=u'undef', shortname=u'undef', abbreviation=u'undef', link=u'undef', thumbnail=u'undef', shareable=u'undef', description=u'undef'):
+        self.name = name
+        self.shortname = shortname
+        self.abbreviation = abbreviation
+        self.link = link
+        self.thumbnail = thumbnail
+        self.shareable = shareable
+        self.description = description
+        
+    def __repr__(self):
+        return "<License(id='%d', name='%s')>" % (self.id, self.name)
+    pass
+
 
 class Talk(Base):
     __tablename__ = 'talks'
 
     id = sa.Column(sa.types.Integer, primary_key=True)
-    short_title = sa.Column(sa.types.Unicode)
-    title = sa.Column(sa.types.UnicodeText)
-    description = sa.Column(sa.types.UnicodeText)
-    thumbnail_url = sa.Column(sa.types.UnicodeText)
-    recording_date = sa.Column(sa.types.Date)
-    license = sa.Column(sa.types.UnicodeText)
-    video_duration = sa.Column(sa.types.Interval)
-    video_embedcode = sa.Column(sa.types.UnicodeText)
-    video_bliptv_id = sa.Column(sa.types.Integer)
+    short_title = sa.Column(sa.types.Unicode())
+    title = sa.Column(sa.types.Unicode())
+    description = sa.Column(sa.types.UnicodeText())
+    thumbnail_url = sa.Column(sa.types.Unicode())
+    recording_date = sa.Column(sa.types.Date())
+    video_duration = sa.Column(sa.types.Interval())
+    video_embedcode = sa.Column(sa.types.UnicodeText())
+    video_bliptv_id = sa.Column(sa.types.Integer())
+
+    ## We need a set list of licenses because they MUST be the same as where the videos are stored.
+    license = orm.relation(license, backref=orm.backref('licenses', order_by=id))
     
     speakers = orm.relation('Speaker', secondary=talks_speakers_table, backref='talks')
 
-    def __init__(self, short_title, title, description, thumbnail_url, recording_date, license, video_duration, video_embedcode, video_bliptv_id):
+    def __init__(self, short_title=u'undef', title=u'undef', description=u'undef', thumbnail_url=u'undef', recording_date=datetime.datetime.now(), license=None, video_duration=datetime.timedelta(0), video_embedcode=u'undef', video_bliptv_id=u'undef'):
         self.short_title = short_title
         self.title = title
         self.description = description
