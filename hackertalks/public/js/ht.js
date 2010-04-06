62c967da5c05315727f8848c1f8fb7ef5269a886
Markus Hametner <fin+github@xbhd.org>
2010-04-06 14:24:39 +0200

    sorta-ugly method to continue playing after absolutize. time-based. this could be better

--- hackertalks/public/js/ht.js
+++ hackertalks/public/js/ht.js
@@ -15,13 +15,15 @@ $(function() {
     $('#duration_end_display').html($('[name=duration_end]')[0].value);
 });
 
+var just_changedlights=false;
+
 var lights = {
     'on': function() {
         document.body.className='';
         $('.overlay').animate({opacity: 0}, 1000, function() { $('.overlay').remove(); });
+        just_changedlights=true;
     },
     'out': function() {
-        document.body.className='lightsoff';
         $('#talk_container').absolutize_keeplayout();
         $('#talk_container').css('z-index', 100);
         if($('#talk_container').css('background-image')) {
@@ -35,18 +37,34 @@ var lights = {
         e.style.width='100%';
         e.style.zIndex='99';
         e.style.position='absolute';
-        e.style.top=0;
+        e.style['top']=0;
         e.style.left=0;
         e.className = 'overlay';
         e.style.backgroundColor='black';
         e.style.opacity=0;
         $(e).animate({opacity: 1});
+        document.body.className='lightsoff';
         document.body.appendChild(e);
+        just_changedlights=true;
     },
     'toggle': function() {
         if(document.body.className=='lightsoff')
             lights.on();
         else
             lights.out();
+    }
+};
+
+var current_second = 0;
+var getUpdate = function(type,arg1,arg2) {
+    if(type=='time') {
+        if(just_changedlights && current_second > arg1) {
+            var current_second_gah = current_second;
+
+            setTimeout(function() { $('#talk_container embed')[0].sendEvent('play'); }, 2000);
+            setTimeout(function() { $('#talk_container embed')[0].sendEvent('scrub', current_second_gah); }, 3000);
+        }
+        just_changedlights=false;
+        current_second = arg1;
     }
 };
