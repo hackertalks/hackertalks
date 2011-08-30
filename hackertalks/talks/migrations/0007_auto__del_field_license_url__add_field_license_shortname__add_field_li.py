39e5aca52dcdffb968908a5672251655785070cd
fin <fin+github@xbhd.org>
2011-08-30 23:58:44 +0200

    migrations

---
+++ hackertalks/talks/migrations/0007_auto__del_field_license_url__add_field_license_shortname__add_field_li.py
@@ -0,0 +1,148 @@
+# encoding: utf-8
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        
+        # Deleting field 'License.url'
+        db.delete_column('talks_license', 'url')
+
+        # Adding field 'License.shortname'
+        db.add_column('talks_license', 'shortname', self.gf('django.db.models.fields.TextField')(default=''), keep_default=False)
+
+        # Adding field 'License.link'
+        db.add_column('talks_license', 'link', self.gf('django.db.models.fields.TextField')(default=''), keep_default=False)
+
+        # Adding field 'License.thumbnail'
+        db.add_column('talks_license', 'thumbnail', self.gf('django.db.models.fields.TextField')(default=''), keep_default=False)
+
+        # Adding field 'License.description'
+        db.add_column('talks_license', 'description', self.gf('django.db.models.fields.TextField')(default=''), keep_default=False)
+
+        # Adding field 'License.shareable'
+        db.add_column('talks_license', 'shareable', self.gf('django.db.models.fields.BooleanField')(default=False), keep_default=False)
+
+        # Changing field 'License.abbreviation'
+        db.alter_column('talks_license', 'abbreviation', self.gf('django.db.models.fields.CharField')(unique=True, max_length=100))
+
+        # Adding field 'Conference.url'
+        db.add_column('talks_conference', 'url', self.gf('django.db.models.fields.CharField')(max_length=50, null=True), keep_default=False)
+
+
+    def backwards(self, orm):
+        
+        # Adding field 'License.url'
+        db.add_column('talks_license', 'url', self.gf('django.db.models.fields.TextField')(default=''), keep_default=False)
+
+        # Deleting field 'License.shortname'
+        db.delete_column('talks_license', 'shortname')
+
+        # Deleting field 'License.link'
+        db.delete_column('talks_license', 'link')
+
+        # Deleting field 'License.thumbnail'
+        db.delete_column('talks_license', 'thumbnail')
+
+        # Deleting field 'License.description'
+        db.delete_column('talks_license', 'description')
+
+        # Deleting field 'License.shareable'
+        db.delete_column('talks_license', 'shareable')
+
+        # Changing field 'License.abbreviation'
+        db.alter_column('talks_license', 'abbreviation', self.gf('django.db.models.fields.CharField')(max_length=50, unique=True))
+
+        # Deleting field 'Conference.url'
+        db.delete_column('talks_conference', 'url')
+
+
+    models = {
+        'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        'auth.permission': {
+            'Meta': {'ordering': "('content_type__app_label', 'content_type__model', 'codename')", 'unique_together': "(('content_type', 'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        'auth.user': {
+            'Meta': {'object_name': 'User'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        'talks.conference': {
+            'Meta': {'object_name': 'Conference'},
+            'admin': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.TextField', [], {}),
+            'slug': ('autoslug.fields.AutoSlugField', [], {'unique': 'True', 'max_length': '100', 'populate_from': 'None', 'unique_with': '()', 'db_index': 'True'}),
+            'url': ('django.db.models.fields.CharField', [], {'max_length': '50', 'null': 'True'})
+        },
+        'talks.license': {
+            'Meta': {'object_name': 'License'},
+            'abbreviation': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '100'}),
+            'description': ('django.db.models.fields.TextField', [], {}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'link': ('django.db.models.fields.TextField', [], {}),
+            'name': ('django.db.models.fields.TextField', [], {}),
+            'shareable': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'shortname': ('django.db.models.fields.TextField', [], {}),
+            'thumbnail': ('django.db.models.fields.TextField', [], {})
+        },
+        'talks.speaker': {
+            'Meta': {'object_name': 'Speaker'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.TextField', [], {}),
+            'slug': ('autoslug.fields.AutoSlugField', [], {'unique': 'True', 'max_length': '100', 'populate_from': 'None', 'unique_with': '()', 'db_index': 'True'}),
+            'title': ('django.db.models.fields.TextField', [], {})
+        },
+        'talks.talk': {
+            'Meta': {'object_name': 'Talk'},
+            'conference': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['talks.Conference']", 'null': 'True'}),
+            'description': ('django.db.models.fields.TextField', [], {}),
+            'duration': ('django.db.models.fields.TimeField', [], {}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'license': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['talks.License']", 'to_field': "'abbreviation'", 'null': 'True'}),
+            'slug': ('autoslug.fields.AutoSlugField', [], {'unique': 'True', 'max_length': '100', 'populate_from': 'None', 'unique_with': '()', 'db_index': 'True'}),
+            'speakers': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['talks.Speaker']", 'symmetrical': 'False'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'video_bliptv_id': ('django.db.models.fields.TextField', [], {}),
+            'video_embedcode': ('django.db.models.fields.TextField', [], {}),
+            'video_image': ('django.db.models.fields.TextField', [], {})
+        },
+        'talks.talkfeature': {
+            'Meta': {'object_name': 'TalkFeature'},
+            'date': ('django.db.models.fields.DateField', [], {'default': 'datetime.datetime(2011, 8, 12, 15, 49, 27, 854160)'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'talk': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['talks.Talk']"})
+        }
+    }
+
+    complete_apps = ['talks']
