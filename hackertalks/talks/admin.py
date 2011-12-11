dfab164a4cc059b1f7ca2c7c78c51943c623bca6
fin <fin+github@xbhd.org>
2011-12-12 00:40:14 +0100

    refresh from blipurl in admin

--- hackertalks/talks/admin.py
+++ hackertalks/talks/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from models import Talk, TalkFeature
 
-admin.site.register(Talk)
+def refresh_from_blip(modeladmin, request, queryset):
+    for x in queryset:
+        x.refresh_from_blip()
+
+class TalkAdmin(admin.ModelAdmin):
+    actions = [refresh_from_blip]
+
+admin.site.register(Talk, TalkAdmin)
 admin.site.register(TalkFeature)
